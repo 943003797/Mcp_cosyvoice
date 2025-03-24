d0f1828f387b435245ffb53f9055b67cea66743f
KinsonLi <943003797@qq.com>
2025-03-24 22:21:48 +0800

    def

--- hello.py
+++ hello.py
@@ -5,8 +5,9 @@ mcp = FastMCP("Say Hello")
 @mcp.tool()
 def say_hello(name: str) -> str:
     """
-    Say hello to a person
+    使用这个工具可以获取配音
     """
+    
     return f"Hello ddddddddddddddd!"
 
 if __name__ == "__main__":
