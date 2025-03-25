3ef4a788213236b4e3657ec96cf3694fd10cc9fc
rt.kinsonli <kinsonli@rayootech.com>
2025-03-25 13:31:15 +0800

    refactor(server): 重命名函数并更新注释以提升代码可读性
    将函数 `say_hello` 重命名为 `create_text_to_audio`，以更准确地反映其功能。同时，更新了函数注释，将其从中文翻译为英文，以提高代码的可维护性和国际化支持。

--- server.py
+++ server.py
@@ -7,42 +7,32 @@ import time
 mcp = FastMCP("GetAudio")
 
 @mcp.tool()
-def say_hello(text: str,file_neme: str, file_path: str) -> str:
+def create_text_to_audio(text: str,file_neme: str, file_path: str) -> str:
     """
-    使用这个工具可以获取配音, 输入文本和目录即可生成配音并保存到指定目录
-    参数:
-        text: 要生成配音的文本
-        file_neme: 文件名
-        file_path: 保存目录
-    返回:
-        Success: 成功
-        Failed: 失败
+    Use this tool to generate voiceover. Input text and directory to generate and save voiceover to the specified directory
+    Parameters:
+        text: Text to generate voiceover from
+        file_neme: File name
+        file_path: Save directory
+    Returns:
+        Success: Operation successful
+        Failed: Operation failed
     """
 
-    dashscope.api_key = 'sk-30d9c41320b94ca09f1eb917261ed379'  # 如果您没有配置环境变量，请在此处用您的API-KEY进行替换
-    url = "https://api.hostize.com/files/d7B0_pGfZZ/download/file.wav"  # 请按实际情况进行替换
+    dashscope.api_key = 'sk-30d9c41320b94ca09f1eb917261ed379'
+    url = "https://api.hostize.com/files/d7B0_pGfZZ/download/file.wav"
     prefix = 'prefix'
     target_model = "cosyvoice-v1"
-    # 创建语音注册服务实例
+
     service = VoiceEnrollmentService()
 
-    # 调用create_voice方法复刻声音，并生成voice_id
-    # voice_id = service.create_voice(target_model=target_model, prefix=prefix, url=url)
-    # print("requestId: ", service.get_last_request_id())
-    # print(f"your voice id is {voice_id}")
     voice_id = "cosyvoice-prefix-4eec46a3b5d8499a8c29c46766452a63"
-
-    # 使用复刻的声音进行语音合成
     synthesizer = SpeechSynthesizer(model=target_model, voice=voice_id)
     audio_result = synthesizer.call(text)
     print("requestId: ", synthesizer.get_last_request_id())
 
-    # 将合成的音频文件保存到本地文件
-    # 定义保存目录
     save_directory = file_path if file_path else ""
-    # 确保保存目录存在
     os.makedirs(save_directory, exist_ok=True)
-    # 拼接完整的文件路径
     file_path = os.path.join(save_directory, f"{file_neme}.mp3")
     try:
         with open(file_path, "wb") as f:
